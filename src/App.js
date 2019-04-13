c7d0a6de80efba0c3edee123dd92f8fd4cc8afe6
Tangent Chang <andy23512@gmail.com>
2019-04-13 10:22:57 +0800

    Add feature data, add import all code in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Reference: https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+function importAll(r) {
+  let data = {};
+  r.keys().map((item, index) => { data[item.replace('./', '')] = r(item); });
+  return data;
+}
+
+const data = importAll(require.context('./data', false, /\.feature$/));
+
+console.log(data)
+// Will get a structure like
+// {data1.feature: "/static/media/data1.ab3c103d.feature", data2.feature: "/static/media/data2.edc3d379.feature"}
+// With this structure, you can use the URLs to fetch the data
+
 class App extends Component {
+  componentDidMount() {
+    Promise.all(
+      Object.values(data).map(url => window.fetch(url).then(response.text()))
+    )
+    .then(console.log)
+  }
   render() {
     return (
       <div className="App">
